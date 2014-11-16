c34ae959588a14adecbc574b331373492110c446
Henry Garcia <hgarc014@ucr.edu>
2014-11-15 22:11:58 -0800

    output redirection is working

--- src/rshell.cpp
+++ src/rshell.cpp
@@ -21,7 +21,7 @@ using namespace boost;
 
 void executeCommand(const string &input, const char cmd[]);
 void createCommand(const string &input,const char cmd[]);
-void input(const string &input, bool is3);
+void inputc(const string &input, bool is3);
 void output(const string &input, bool isAp);
 void piping(const string &input);
 
@@ -68,14 +68,17 @@ int main(){
         else if(input.find("|") != string::npos)
         {
             cout << "Do something for piping" << endl;
+            piping(input);
         }
         else if (input.find("<") != string::npos)
         {
             cout << "found < only" << endl;
+            inputc(input,false);
         }
         else if (input.find(">>") != string::npos)
         {
             cout << "found >> only" << endl;
+            output(input,true);
         }
         else if (input.find(">") != string::npos)
         {
@@ -146,33 +149,171 @@ void executeCommand(const string &input,const char cmd[]){
         delete [] argv[i];
 }
 
-void input(const string &input, bool is3)
+void inputc(const string &input, bool is3)
 {
-
+    if(is3)
+        ;
+    char *argv[SZ];
+    int fd = open("file",O_RDONLY);
+    //int oldstd = dup(0);
+    if(close(0) == -1)
+    {
+        perror("close");
+        exit(1);
+    }
+    if(dup(fd)==-1)
+    {
+        perror("dup");
+        exit(1);
+    }
+    argv[0] = new char[40];
+    argv[1] = NULL;
+    strcpy(argv[0],"cat");
+    execvp(argv[0],argv);
+    perror("execvp");
 }
 
 void output(const string &input, bool isAp)
 {
+    string val;
+    char *argv[SZ];
     int redirect = 1;
+    int flags = O_RDWR|O_CREAT;
     if(input.find("2>") != string::npos)
         redirect = 2;
-    int fd = open("testfile",O_RDWR|O_CREAT);
+    if(isAp)
+    {
+        val = ">>";
+        flags |= O_APPEND;
+    }
+    else
+    {
+        val = ">";
+        flags |= O_TRUNC;
+    }
+    string l = input.substr(0,input.find(val));
+    string r = input.substr(input.find(val)+val.size());
+    //    cout << "l=" << l << endl
+    //        << "r=" << r << endl;
+    SEP sep(" ");
+    TOKEN tok(l,sep);
+    TOKEN tok2(r,sep);
+    TOKEN::iterator it = tok.begin();
+    TOKEN::iterator it2 = tok2.begin();
+    int i;
+    for(i =0;it != tok.end(); ++it,++i)
+    {
+        argv[i] = new char[(*it).size()];
+        strcpy(argv[i],(*it).c_str());
+    }
+    argv[i]=NULL;
+
+    if(it2 == tok2.end())
+    {
+        cout << "ERROR NO FILE" << endl;
+        return;
+    }
+    int fd = open((*it2).c_str(),flags,S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH);
+    if(fd ==-1)
+    {
+        perror("open");
+        exit(1);
+    }
+    if(argv[0] == NULL)
+    {
+        cout << "no command" << endl;
+        close(fd);
+        return;
+    }
     int oldstd = dup(redirect);
-    close(redirect);
-    dup(fd);
-    cout << "i just wrote to the file " << endl;
-    char *argv[10];
-        strcpy(argv[0],"ls");
-    argv[1]=NULL;
+    if(oldstd == -1)
+    {
+        perror("dup");
+        exit(1);
+    }
+    if(close(redirect)==-1)
+    {
+        perror("close");
+        exit(1);
+    }
+    if(dup(fd)==-1)
+    {
+        perror("dup");
+        exit(1);
+    }
+    //    cout << "should be deleted!!!" << endl;
+    //    char *argv[10];
+    //    argv[0]=new char[40];
+    //    strcpy(argv[0],"l");
+    //    argv[1]=NULL;
     execvp(argv[0],argv);
-    close(redirect);
-    dup(oldstd);
-    cout << "made it back, but I shouldn't have" << endl;
+    perror("execvp");
     exit(1);
+    oldstd = oldstd;
 }
 
 void piping(const string &input)
 {
-
+    int fd[2];
+    if(pipe(fd)==-1)
+    {
+        perror("pipe");
+        exit(1);
+    }
+    int pid=fork();
+    if(pid == -1)
+    {
+        perror("FORK");
+        exit(1);
+    }
+    else if (pid == 0)
+    {
+        char *argv[SZ];
+        argv[0]= new char[40];
+        strcpy(argv[0],"ls");
+        cout << "executing ls" << endl;
+        argv[1]= NULL;
+        if(close(1)==-1)
+        {
+            perror("close");
+            exit(1);
+        }
+        if(dup(fd[1])==-1)
+        {
+            perror("dup");
+        }
+        execvp(argv[0],argv);
+        perror("execvp");
+    }
+    else
+    {
+        if(wait(NULL)==-1)
+        {
+            perror("WAIT");
+            exit(1);
+        }
+        if(close(fd[1])==-1)
+        {
+            perror("close");
+            exit(1);
+        }
+        if(close(0)==-1)
+        {
+            perror("close");
+            exit(1);
+        }
+        if(dup(fd[0])==-1)
+        {
+            perror("dup");
+        }
+        char *argv[SZ];
+        argv[0] = new char[40];
+        argv[1] = new char[40];
+        strcpy(argv[0],"grep");
+        strcpy(argv[1],".rem");
+        argv[2]=NULL;
+        execvp(argv[0],argv);
+        perror("execvp");
+    }
 }
 
